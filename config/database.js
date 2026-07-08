eba37d019c57631076b4ce5d396f03413b5afd84
Abubeker Adem Seid <allencollinsh1@gmail.com>
2026-07-08 03:46:28 +0300

    Add database schema and connection setup

---
+++ config/database.js
@@ -0,0 +1,50 @@
+const Database = require('better-sqlite3');
+const db = new Database('taskmatch.db');
+
+db.exec(`
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password _hash TEXT NOT NULL,
+    role TEXT NOT NULL CHECK(role IN('client', 'worker')),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id INTEGER NOT NULL,
+    title TEXT NOT NULL, 
+    description TEXT NOT NULL,
+    budget REAL NOT NULL, 
+    deadline DATE,
+    status TEXT NOT NULL DEFAULT 'open' CHECK(status IN('open', 'assigned', 'in_progress', 'completed')),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (client_id) REFERENCES users(id)
+);
+CREATE TABLE IF NOT EXISTS applications(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task_id INTEGER NOT NULL,
+    worker_id INTEGER NOT NULL, 
+    proposal_text TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'pending' CHECK(status IN ('pending', 'accepted', 'rejected')),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (worker_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task_id INTEGER NOT NULL,
+    reviewer_id INTEGER NOT NULL,
+    reviewee_id INTEGER NOT NULL,
+    rating INTEGER NOT NULL CHECK(rating BETWEEN 1 and 5),
+    comment TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (reviewer_id) REFERENCES users(id),
+    FOREIGN KEY (reviewee_id) REFERENCES users(id)
+    );
+    `);
+
+    module.exports = db;
---
+++ test-db.js
@@ -0,0 +1,2 @@
+const db = require('./config/database');
+console.log('Database connected successfully and tables are created successfully!');
